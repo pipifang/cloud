944232669ed8575197744270539c4668e87379b0
pipifang <1658119598@qq.com>
2021-04-07 00:09:42 +0800

    Initial commit

---
+++ cloud-provider-payment8081/src/main/java/com/zcg/cloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.zcg.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author : zcg
+ * @Data ：Create in 23:462021/4/6
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
